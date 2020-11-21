eb3a659cc8077630f267a46e2c0b7978e8071eb2
Timothy Muresan <muresan_timothy@yahoo.com>
2020-11-20 19:28:40 -0800

    Modified wordCount to return sortedMap

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,8 +1,16 @@
 import org.junit.Test;
 
+import java.util.Set;
+import java.util.TreeMap;
+
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
         HandleTruth.wordCount();
+    }
+
+    @Test
+    public void realTest() {
+        TreeMap<Integer, Set<String>> actual = HandleTruth.wordCount();
     }
 }
