0b9530414ffacea0d2a25150afa032208dfde178
Timothy Muresan <muresan_timothy@yahoo.com>
2020-11-21 11:14:35 -0800

    optimized testing a little with Arrays.asList

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -7,7 +7,9 @@ import static org.junit.Assert.assertEquals;
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
-        String msg = "son we live in a world that has walls and those walls have to be guarded by men with guns whos gonna do it you you lieutenant weinberg i have a greater responsibility than you can possibly fathom you weep for santiago and you curse the marines you have that luxury you have the luxury of not knowing what i know that santiagos death while tragic probably saved lives and my existence while grotesque and incomprehensible to you saves lives you dont want the truth because deep down in places you dont talk about at parties you want me on that wall you need me on that wall we use words like honor code loyalty we use these words as the backbone of a life spent defending something you use them as a punchline i have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket of the very freedom that i provide and then questions the manner in which i provide it i would rather you just said thank you and went on your way otherwise i suggest you pick up a weapon and stand a post either way i dont give a damn what you think you are entitled to";
+        String msg = "in the beginning was the Word and the Word was with God and the Word was God";
+
+        //"son we live in a world that has walls and those walls have to be guarded by men with guns whos gonna do it you you lieutenant weinberg i have a greater responsibility than you can possibly fathom you weep for santiago and you curse the marines you have that luxury you have the luxury of not knowing what i know that santiagos death while tragic probably saved lives and my existence while grotesque and incomprehensible to you saves lives you dont want the truth because deep down in places you dont talk about at parties you want me on that wall you need me on that wall we use words like honor code loyalty we use these words as the backbone of a life spent defending something you use them as a punchline i have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket of the very freedom that i provide and then questions the manner in which i provide it i would rather you just said thank you and went on your way otherwise i suggest you pick up a weapon and stand a post either way i dont give a damn what you think you are entitled to";
         HandleTruth.wordCount(msg);
     }
 
@@ -15,8 +17,8 @@ public class HandleTruthTest {
     public void realTest() {
         String[] msg = {
                 "Hello I am the mail man",
-                "Luke I am your father",
-                "hello there hello how are you hello",
+                "therefore I say to you whatever things you ask when you pray believe that you receive them and you will have them",
+                "in the beginning was the Word and the Word was with God and the Word was God",
                 "this is this and that is that",
                 "no no no just no",
                 "drift drift drift drift drift",
@@ -32,18 +34,19 @@ public class HandleTruthTest {
     }
     private static List<TreeMap<Integer, Set<String>>> getTestCases() {
         List<TreeMap<Integer, Set<String>>> testCases = new ArrayList<>();
+        
         //Test case 1
         TreeMap<Integer, Set<String>> expected1 = new TreeMap<>();
-        Set<String> string1 = new HashSet<>();
-        string1.add("Hello");
-        string1.add("I");
-        string1.add("am");
-        string1.add("the");
-        string1.add("mail");
-        string1.add("man");
-        expected1.put(1, string1);
+        expected1.put(1, new HashSet<>(Arrays.asList("Hello", "I", "am", "the", "mail", "man")));
         testCases.add(expected1);
-        
+
+        //Test case 2
+        TreeMap<Integer, Set<String>> expected2 = new TreeMap<>();
+        expected2.put(5, new HashSet<>(Arrays.asList("you")));
+        expected2.put(2, new HashSet<>(Arrays.asList("them")));
+        expected2.put(1, new HashSet<>(Arrays.asList("receive", "will", "I", "say", "believe", "when", "whatever", "pray", "that", "and", "things", "ask", "have", "therefore", "to")));
+        testCases.add(expected2);
+
         return testCases;
     }
 }
